2b0da1d9a732c8169b940406720ccd4c57f582ef
tpiwo <tpiwonski@gmail.com>
2018-04-29 22:11:04 +0200

    reportlab test

--- generator/urls.py
+++ generator/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('pdfkit/', views.test, name='test_pdfkit'),
+    path('', views.test_reportlab, name='test_reportlab')
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from io import BytesIO
+
 import pdfkit
+
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, B5, landscape
+from reportlab.lib.units import mm
 
 # Create your views here.
 from django.template.loader import render_to_string
@@ -31,3 +37,22 @@ def test(request):
     return HttpResponse(content, content_type='application/pdf')
 
     # return render(request, 'generator/test.html', context=context)
+
+def test_reportlab(request):
+    buffer = BytesIO()
+    pdf = canvas.Canvas(buffer)
+    for i in range(0, 2):
+        # image = Image.open(r'C:\Users\tpiwo\Projects\pdftest\generator\static\images\sample.jpg')
+        pdf.setPageSize(landscape(letter))
+        pdf.drawImage(r'C:\Users\tpiwo\Projects\pdftest\generator\static\images\sample.jpg', 0, 0)
+        pdf.drawString(30, 100, 'lorem ipsum')
+        pdf.showPage()
+
+    pdf.save()
+    content = buffer.getvalue()
+    buffer.close()
+
+    response = HttpResponse(content_type='application/pdf')
+    # response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+    response.write(content)
+    return response
